2a713fa76ce090f3a44bb03b29153bee3b20e018
mayu0126 <ildikopinter0126@gmail.com>
2023-08-16 00:07:43 +0200

    Started to develop the UI

---
+++ Frontend/Grande/src/Pages/Layout/Layout.jsx
@@ -0,0 +1,34 @@
+import { Outlet, Link } from "react-router-dom";
+
+import "./Layout.css";
+
+const Layout = () => (
+    <div className="Layout">
+        <nav>
+            <ul>
+                <li className="home">
+                    <Link to="/" className="link">THE EVERYTHING</Link>
+                </li>
+                <div className="menu-container">
+                    <li className="menu">
+                        <Link to="/products" className="link">Products</Link>
+                    </li>
+                    <li className="menu">
+                        <Link to="/about_us" className="link">About Us</Link>
+                    </li>
+                    <li className="menu">
+                        <Link to="/feedbacks" className="link">Feedbacks</Link>
+                    </li>
+                </div>
+                <div className="button-container">
+                    <Link to="/login">
+                        <button type="button">Log In</button>
+                    </Link>
+                </div>
+            </ul>
+        </nav>
+    <Outlet />
+    </div>
+);
+
+export default Layout;
--- Frontend/Grande/src/main.jsx
+++ Frontend/Grande/src/main.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Layout from "./Pages/Layout";
 import './index.css'
 import GoodsList from './Pages/GoodsList.jsx'
 import CustomerList from './Pages/CustomerList'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    //errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <CustomerList />,
+      },
+      {
+        path: "/",
+        element: <GoodsList />,
+      },
+    ],
+  },
+]);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <CustomerList />
-    <GoodsList />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
