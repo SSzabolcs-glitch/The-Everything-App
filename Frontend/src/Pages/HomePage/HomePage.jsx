f5cbdf59bdfc10ddd638d446646e892f1748ff8a
mayu0126 <ildikopinter0126@gmail.com>
2023-09-14 00:49:22 +0200

    Home page improvement - what we offer part

--- Frontend/src/Pages/HomePage/HomePage.jsx
+++ Frontend/src/Pages/HomePage/HomePage.jsx
@@ -14,12 +14,48 @@ function HomePage() {
             <button className="button-get-started">Get Started</button>
           </div>
         </div>
-        <div className="main-picture"></div>
+        <div className="main-picture-container">
+          <div className="main-picture"></div>
+        </div>
         </div>
 
       </div>
       
-      <div className="what-we-offer">What we offer</div>
+      <div className="what-we-offer-content">
+        <div className="what-we-offer-text">What we offer</div>
+        <div className="what-we-offer-container">
+          <div className="what-we-offer-picture-and-text-container-1">
+            <div className="what-we-offer-picture-1"></div>
+            <div className="what-we-offer-text-1">
+              Every project is unique, which is why we offer flexible rental and purchase options.
+              Whether you need a tool for a day or two weeks, our rental periods cater to your specific
+              requirements. Alternatively, if you find a tool you can&apos;t live without, you can choose
+              to buy it and have it as a permanent addition to your toolkit. Your needs, your choices -
+              that&apos;s the The Everything way.
+            </div>
+          </div>
+          <div className="what-we-offer-picture-and-text-container-2">
+            <div className="what-we-offer-picture-2"></div>
+            <div className="what-we-offer-text-2">
+              Your safety is paramount to us. That&apos;s why we take pride in providing only the highest quality
+              and meticulously maintained tools in our inventory. Every tool undergoes rigorous safety checks
+              and maintenance procedures to guarantee your peace of mind while working on your projects.
+              With The Everything, you can trust that your tools are as safe as they are effective, making
+              your DIY experience not only productive but also worry-free.
+            </div>
+          </div>
+          <div className="what-we-offer-picture-and-text-container-3">
+            <div className="what-we-offer-picture-3"></div>
+            <div className="what-we-offer-text-3">
+              We understand that time is of the essence when you have a project in mind.
+              That&apos;s why we offer lightning-fast one-day order delivery, ensuring that you can get started
+              on your DIY endeavor without delay. Say goodbye to waiting around for your tools, and hello
+              to efficiency and productivity. We&apos;re here to make your projects happen when you need them most.
+            </div>
+          </div>
+        </div>
+
+      </div>
     </div>
     );
 }
