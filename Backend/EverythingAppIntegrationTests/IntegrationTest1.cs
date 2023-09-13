089a6e76030952908abc9d793afc66e76e292752
Évi <beraevi@gmail.com>
2023-09-13 12:02:20 +0200

    add integration tests for the two freely usable get endpoints

--- Backend/EverythingAppIntegrationTests/IntegrationTest1.cs
+++ Backend/EverythingAppIntegrationTests/IntegrationTest1.cs
@@ -1,35 +1,50 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace EverythingAppIntegrationTests
 {
     public class BasicTests
     {
-        private readonly TestServer _server;
-        private readonly HttpClient _client;
+        private WebApplicationFactory<Program> _factory;
+        private HttpClient _client;
+        
 
-        public BasicTests()
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
         {
-            // Arrange
-            _server = new TestServer(new WebHostBuilder()
-               .UseStartup<Program>());
-            _client = _server.CreateClient();
+            // Create a WebApplicationFactory with your Program class
+            _factory = new WebApplicationFactory<Program>();
+
+            // Create a HttpClient instance for making requests
+            _client = _factory.CreateClient();
         }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            // Dispose of resources after all tests have run
+            _factory.Dispose();
+        }
+
         [SetUp]
         public void Setup()
         {
         }
 
-        [Theory]
-        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        [TestCase("api/Product/GetAll")]
+        [TestCase("api/Product/GetSpecificProduct?name=pen")]
+        public async Task GetEndpointsReturn_SuccessAndCorrectContentType(string url)
         {
             // Act
             var response = await _client.GetAsync(url);
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equals("text/html; charset=utf-8",
+            Assert.AreEqual("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
     }
