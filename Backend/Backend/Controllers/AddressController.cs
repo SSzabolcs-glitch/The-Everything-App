941ae4805507d9f9558755b31e1659fb1f410004
Évi <beraevi@gmail.com>
2023-09-28 17:32:14 +0200

    add AddressController with endpoints to get data by Id and register new

--- Backend/Backend/Controllers/AddressController.cs
+++ Backend/Backend/Controllers/AddressController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Models;
+using Backend.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Data;
 
 namespace Backend.Controllers
 {
@@ -7,5 +12,50 @@ namespace Backend.Controllers
     [ApiController]
     public class AddressController : ControllerBase
     {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IAddressRepository _addressRepository;
+
+        public AddressController(ILogger<ProductController> logger, IAddressRepository addressRepository)
+        {
+            _logger = logger;
+            _addressRepository = addressRepository;
+        }
+
+        [HttpGet("GetAddressById"), Authorize(Roles = "Admin")]
+        public async Task<ActionResult<Address>> GetAddressByIdAsync(int id)
+        {
+            try
+            {
+                var address = await _addressRepository.GetById(id);
+
+                if(address == null)
+                {
+                    return NotFound($"Address with id {id} not exists");
+                }
+                return Ok(address);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error getting address", ex.Message);
+                return BadRequest("Error getting address");
+            }
+
+        }
+
+        [HttpPost("RegisterNewAddress"), Authorize(Roles = "Admin, User")]
+        public async Task<ActionResult<Address>> CreateAddress([FromBody] Address address)
+        {
+            try
+            {
+                var newAddress = await _addressRepository.AddAddressAsync(address);
+                return Ok(newAddress);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error registering new address", ex.Message);
+                return BadRequest("Error registering new address");
+            }
+        }
+
     }
 }
